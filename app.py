1159fbcf1879270bc388e627b2567a67889a7ba0
Shivam Sharma <shivam.usharma@gmail.com>
2022-04-12 18:36:17 +0530

    In Progress

---
+++ app.py
@@ -0,0 +1,8 @@
+from resume import app
+
+if __name__ ==  '__main__':
+    app.run(host="0.0.0.0",debug=True)
+    """
+     host="0.0.0.0" will make the page accessableby going to http://[ip]:5000/ on 
+     any computer in the network.
+    """
---
+++ resume/__init__.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from datetime import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import subprocess as sp
+from flask_mail import Mail
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+
+
+app =  Flask(__name__)
+
+app.config['SECRET_KEY'] = os.urandom(32).hex() #Prevents XSS
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ["EMAIL_USER"]
+app.config["MAIL_PASSWORD"] = os.environ["EMAIL_PASS"]
+mail = Mail(app)
+
+db = SQLAlchemy(app)
+db.create_all()
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'   #provide login path first
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+
+
+from resume import routes
